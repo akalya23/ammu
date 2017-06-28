4bac7577bb4c37c97e95d58c6a62ff16d1c024a2
akalya23 <akalyaammu26@gmail.com>
2017-06-28 14:50:38 +0530

    Create AMMU.C

---
+++ AMMU.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\ enter the number ");
+scanf("%d"&n);
+{
+if(n<0)
+printf("given number n is positive");
+{
+if(n>0)
+printf("given number n is negative");
+{
+if(n==0)
+printf("given numbe rn is zero);
+}
+getch();
+}
